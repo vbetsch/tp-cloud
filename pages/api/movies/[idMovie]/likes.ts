ff6c34b0ca707537f81b32f5723a9a9148a52e90
vbetsch <vic.betsch@gmail.com>
2024-03-14 17:30:48 +0100

    :sparkles: Unlike movie

--- pages/api/movies/[idMovie]/likes.ts
+++ pages/api/movies/[idMovie]/likes.ts
@@ -4,6 +4,11 @@ import { findOneLikeById, insertOneLike, updateOneLikeById } from '../../../../s
 import { LikeType } from '../../../../src/types/firebase/LikeType';
 import { HttpMethods } from '../../../../src/types/HttpMethods';
 
+enum LikesActions {
+	LIKE = 'like',
+	UNLIKE = 'unlike',
+}
+
 /**
  * @swagger
  * /api/movies/{idMovie}/likes:
@@ -30,14 +35,25 @@ import { HttpMethods } from '../../../../src/types/HttpMethods';
  *         schema:
  *           type: string
  *         description: ID movie
+ *       - in: query
+ *         name: action
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Action
  *     responses:
  *       201:
  *         description: Success Response
+ *       400:
+ *         description: Parameter 'action' is required
+ *       405:
+ *         description: Action not allowed
  *       500:
  *         description: Internal Server Error
  */
 export default async function handler(req: NextApiRequest, res: NextApiResponse): Promise<void> {
 	const idMovie: number = parseInt(req.query.idMovie as string, 10);
+	const action: string = req.query.action as string;
 
 	let like: LikeType | null;
 	let errorMessage: string;
@@ -52,9 +68,27 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 			}
 
 			if (like) {
+				if (!action) {
+					errorMessage = "Parameter 'action' is required";
+					console.error(`ERROR: ${errorMessage}`);
+					return res.status(400).json({ error: errorMessage });
+				}
 				try {
+					let delta: number;
+					switch (action) {
+						case LikesActions.LIKE:
+							delta = 1;
+							break;
+						case LikesActions.UNLIKE:
+							delta = -1;
+							break;
+						default:
+							errorMessage = 'Action not allowed';
+							console.error(`ERROR: ${errorMessage}`);
+							return res.status(405).json({ error: errorMessage });
+					}
 					const resMongo: UpdateResult = await updateOneLikeById(idMovie, {
-						$inc: { likeCounter: 1 },
+						$inc: { likeCounter: delta },
 					});
 					if (resMongo.modifiedCount > 1) {
 						console.warn(`WARNING: ${resMongo.modifiedCount} items have been modified`);
@@ -63,7 +97,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 						action: 'likeCounter incremented',
 						idMovie: idMovie,
 						previousValue: like.likeCounter,
-						newValue: like.likeCounter + 1,
+						newValue: like.likeCounter + delta,
 					});
 				} catch (e) {
 					errorMessage = 'Unable to update like';
