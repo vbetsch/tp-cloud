f886432bc3aa12014d880e652b077cd96a2e2f46
vbetsch <vic.betsch@gmail.com>
2024-03-15 09:10:58 +0100

    :truck: Move `firebase.ts` in `src` folder

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,4 +1,4 @@
-import clientPromise from './lib/mongodb';
+import clientPromise from '../lib/mongodb';
 import { Db, MongoClient } from 'mongodb';
 
 const FIREBASE_DATABASE_NAME: string = 'cluster';
