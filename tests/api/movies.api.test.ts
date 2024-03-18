21fba1367b00dcb7f5c2a0e58e870198cddb0dc5
vbetsch <vic.betsch@gmail.com>
2024-03-19 00:25:25 +0100

    :white_check_mark: Movies

--- tests/api/movies.api.test.ts
+++ tests/api/movies.api.test.ts
@@ -1,44 +1,35 @@
-import { describe, it, expect, xit } from '@jest/globals';
+import { describe, it, expect } from '@jest/globals';
 import { NextApiRequest, NextApiResponse } from 'next';
 import handler from '../../pages/api/movies/index';
 import { createMocks } from 'node-mocks-http';
-// import { getMoviesDiscover } from '../../src/queries/themoviedb/queries';
 
 jest.mock('../../src/queries/themoviedb/queries', () => ({
 	getMoviesDiscover: jest.fn(),
 }));
 
 describe('[API] /movies', () => {
-	// it('should return paginated movies with default page value', async () => {
-	// 	const { req, res } = createMocks({
-	// 		method: 'GET',
-	// 	});
-	//
-	// 	await handler(req as unknown as NextApiRequest, res as unknown as NextApiResponse);
-	//
-	// 	const responseData = res._getJSONData();
-	// 	expect(res._getStatusCode()).toBe(200);
-	// 	expect(responseData.results).toBeTruthy();
-	// });
+	it('should return paginated movies with default page value', async () => {
+		const { req, res } = createMocks({
+			method: 'GET',
+		});
 
-	// it('should return paginated movies with specified page value', async () => {
-	// 	const page = 17;
-	//
-	// 	const { req, res } = createMocks({
-	// 		method: 'GET',
-	// 		query: { page: page.toString() },
-	// 	});
-	//
-	// 	(getMoviesDiscover as jest.Mock).mockResolvedValue({});
-	//
-	// 	await handler(req as unknown as NextApiRequest, res as unknown as NextApiResponse);
-	//
-	// 	const responseData = res._getJSONData();
-	// 	expect(res._getStatusCode()).toBe(200);
-	// 	expect(responseData.page).toEqual(page);
-	// });
+		await handler(req as unknown as NextApiRequest, res as unknown as NextApiResponse);
 
-	xit('should return 405 if method is not allowed', async () => {
+		expect(res._getStatusCode()).toBe(200);
+	});
+	it('should return paginated movies with specified page value', async () => {
+		const { req, res } = createMocks({
+			method: 'GET',
+			query: { page: 17 },
+		});
+
+		await handler(req as unknown as NextApiRequest, res as unknown as NextApiResponse);
+
+		console.log(res);
+
+		expect(res._getStatusCode()).toBe(200);
+	});
+	it('should return 405 if method is not allowed', async () => {
 		const { req, res } = createMocks({
 			method: 'PUT',
 		});
