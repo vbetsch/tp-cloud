4992e19dbd9a68aa2f5e6c7b7718c64f2dd826e7
vbetsch <vic.betsch@gmail.com>
2024-03-22 01:19:39 +0100

    :white_check_mark: Complete idMovie test

--- tests/api/movies/idMovie/idMovie.test.ts
+++ tests/api/movies/idMovie/idMovie.test.ts
@@ -3,35 +3,33 @@ import { createMocks } from 'node-mocks-http';
 import handler from '../../../../pages/api/movies/[idMovie]/index';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getMovieById } from '../../../../src/queries/themoviedb/queries';
+import { findOneLikeById } from '../../../../src/queries/mongodb/queries';
+
+const movieId: number = 123;
+const counterLike: number = 5;
 
 jest.mock('../../../../src/queries/themoviedb/queries', () => ({
 	getMovieById: jest.fn(),
 }));
 
-jest.mock('../../../../lib/mongodb', () => ({
-	__esModule: true,
-	default: {
-		db: jest.fn(() => ({
-			collection: jest.fn(() => ({
-				findOne: jest.fn(async () => ({
-					idTMDB: 123,
-					likeCounter: 5,
-				})),
-			})),
-		})),
-	},
+jest.mock('../../../../src/queries/mongodb/queries', () => ({
+	findOneLikeById: jest.fn(),
 }));
 
 describe('[API] /movies/{idMovie}', () => {
 	it('should return movie with likes', async () => {
-		const movieId: number = 123;
+		(getMovieById as jest.Mock).mockResolvedValue({
+			id: movieId,
+		});
 
-		(getMovieById as jest.Mock).mockResolvedValue({});
+		(findOneLikeById as jest.Mock).mockResolvedValue({
+			idTMDB: movieId,
+			likeCounter: counterLike,
+		});
 
 		const { req, res } = createMocks({
 			method: 'GET',
-			url: `/api/movies/${movieId}`,
-			query: { idMovie: movieId.toString() },
+			query: { idMovie: movieId },
 		});
 
 		await handler(req as unknown as NextApiRequest, res as unknown as NextApiResponse);
@@ -40,7 +38,35 @@ describe('[API] /movies/{idMovie}', () => {
 		expect(res._isEndCalled()).toBeTruthy();
 
 		const responseData = res._getJSONData();
-		expect(responseData.likes).toBeDefined();
+		expect(responseData.id).toBe(movieId);
+		expect(responseData.likes).toBe(counterLike);
+	});
+	it("should don't return movie with 0 likes", async () => {
+		const _error = {
+			success: false,
+			status_code: 34,
+			status_message: 'The resource you requested could not be found.',
+		};
+
+		(getMovieById as jest.Mock).mockResolvedValue(_error);
+		(findOneLikeById as jest.Mock).mockResolvedValue(null);
+
+		const { req, res } = createMocks({
+			method: 'GET',
+			query: { idMovie: 41 },
+		});
+
+		await handler(req as unknown as NextApiRequest, res as unknown as NextApiResponse);
+
+		expect(res._getStatusCode()).toBe(200);
+
+		const { success, status_code, status_message } = _error;
+		const responseData = res._getJSONData();
+
+		expect(responseData.success).toBe(success);
+		expect(responseData.status_code).toBe(status_code);
+		expect(responseData.status_message).toBe(status_message);
+		expect(responseData.likes).toBe(0);
 	});
 	it('should return 405 if method is not allowed', async () => {
 		const { req, res } = createMocks({
