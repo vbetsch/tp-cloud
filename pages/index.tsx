ba60b8de996a2c5e33fa22d0f7d551b7cdc6c1fe
vbetsch <vic.betsch@gmail.com>
2024-03-24 23:08:19 +0100

    :sparkles: HomePage router

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import { redirect } from 'next/navigation';
 
 export default function Home(): JSX.Element {
-	return <p>Hello</p>;
+	return (
+		<div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+			<h1>Links</h1>
+			<a href="http://localhost:3000/api/movies">API</a>
+			<a href="http://localhost:3000/ui">UI</a>
+		</div>
+	);
 }
