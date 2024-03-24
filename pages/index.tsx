0e584b45617c8b5d293a043a9b7530ac15b34263
vbetsch <vic.betsch@gmail.com>
2024-03-24 23:41:45 +0100

    :sparkles: Add Swagger link

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ export default function Home(): JSX.Element {
 		<div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
 			<h1>Links</h1>
 			<a href="http://localhost:3000/api/movies">API</a>
+			<a href="http://localhost:3000/docs">Swagger</a>
 			<a href="http://localhost:3000/ui">UI</a>
 		</div>
 	);
